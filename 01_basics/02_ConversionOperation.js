0ce231932f50e747047dd050af6d22143d05f90c
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-07 18:57:32 +0000

    Conversion of Data Types

---
+++ 01_basics/02_ConversionOperation.js
@@ -0,0 +1,8 @@
+let number = 33;
+let umbrella = Boolean(number);
+let Hat = String(number);
+console.log(typeof number);
+console.log(typeof umbrella);
+console.log(typeof Hat);
+console.log(umbrella);
+console.log(Hat);
