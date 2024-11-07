fa1b45ca56f67a43614b2bd07acf0d2a2636c3ba
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-07 18:07:26 +0000

    changes are comitted

---
+++ 01_basics/01variables.js
@@ -0,0 +1,6 @@
+const accountId = 12212;
+let accountEmail = "diamondtyagi123@gmail.com";
+var accountName = "Harry";
+accountEmail = "god46630@gmail.com";
+accountName = "Perry";
+console.table([accountId, accountName, accountEmail]);
