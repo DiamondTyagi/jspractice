04af12609c294b20d92104659cd4768b2c166187
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-08 13:23:27 +0000

    MathNum

---
+++ 01_basics/05Math.js
@@ -0,0 +1,17 @@
+/*const balance = new Number(100.45567);
+console.log(balance);
+
+/*console.log(balance.toString().length);
+console.log(balance.toFixed(2));//fix to 2 decimal places from behind.
+console.log(balance.toPrecision(4));*/
+/*const hundreds = 10000000
+console.log(hundreds.toLocaleString("en-IN"));*/
+/*console.log(Math.abs(-52));
+console.log(Math.round(5.54));
+console.log(Math.ceil(5.6));
+console.log(Math.floor(5.4));*/
+
+console.log(Math.random());
+const Max = 10;
+const Min = 20;
+console.log(Math.floor(Math.random()* (Max - Min + 1)) + Min);//Algo that might be used in a lottery game or a randomised game.
