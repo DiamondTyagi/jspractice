d6c3ab2b2bc50a92b30d32c5be065e9c0a3889a3
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-08 08:18:05 +0000

    String Completede

---
+++ 01_basics/04Strings.js
@@ -0,0 +1,16 @@
+/*const Mom = "Geeta";
+console.log(Mom.length);
+console.log(Mom.toUpperCase());
+console.log(Mom.charAt(4));
+console.log(Mom.indexOf("e"));
+
+const Mother = Mom.substring(0, 3);
+console.log(Mother);
+const Maa = Mom.slice(-8, 4);
+console.log(Maa);*/
+let newvariable = "    hello      ";
+let variable = newvariable;
+console.log(newvariable);
+console.log(variable.replace("h", "l"));
+console.log(variable.includes("sundar"));
+console.log(variable.split("l"));
