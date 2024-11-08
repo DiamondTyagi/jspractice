9a310018df01c7e173b370059cadc1fb2f3a3d37
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-08 14:07:51 +0000

    Date and time to be revised

---
+++ 01_basics/06DataandTime.js
@@ -0,0 +1,12 @@
+/*let MyDate = new Date();
+console.log(MyDate);
+console.log(MyDate.toString());
+console.log(MyDate.toDateString());
+console.log(MyDate.toLocaleDateString());
+console.log(typeof MyDate);*/
+
+let MyCreatedDate = new Date("2024-02-20");
+//console.log(MyCreatedDate.toLocaleString());
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(MyCreatedDate.getTime());
