e68af4a8b7cef6fc9a8e9807088e85e281e68cf1
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-09 12:29:39 +0000

    Arrowsandthiskeyword

---
+++ 03_basics/03_Arrows.js
@@ -0,0 +1,28 @@
+//user = {
+   /* name: "Harry",
+    price: 999,
+    welcomemessage: function(){
+       console.log(`${this.name}, how are you?`);
+       console.log(this);
+    }
+//}
+//user.name = "Diamond";
+//user.welcomemessage();
+console.log(this);*/
+/*const chai = function(){
+    let username = "hitesh"
+    console.log(this);
+}
+chai();*///undefined answer with this.name and a lot of things answer as "this"
+
+//+++++++++++++++Arrow function
+//const chat = () => {
+ /*   let username = "Sameer";
+    console.log(this);
+}*/
+//chat();// {} is the answer with this keyword in arrow function
+
+const addTwo = (num1, num2) => (num1 + num2);
+const Two = (num3, num4) => ({username: "Diamond"});
+console.log(addTwo(15,20));
+console.log(Two(3,4));
