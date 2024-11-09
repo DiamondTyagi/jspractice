8ead902658a7dc79ec4e859214c3e01093ab0835
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-09 14:13:23 +0000

    ControleStatements

---
+++ 04controlflow.js/one.js
@@ -0,0 +1,10 @@
+/*const balance = 1000;
+if(balance < 1368) console.log("less that require balance"),
+console.log("can't sign-in");*/
+//short hand implementation of the if-statement.
+const userloggedin = true;
+const debitcarddetails = true;
+if(userloggedin && debitcarddetails && 2 == 3){
+    console.log("user is signed in");
+    
+}
---
+++ 04controlflow.js/switch.js
@@ -0,0 +1,26 @@
+/*const month = 13;
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+
+    default:
+        console.log("Invalid month");
+        break;
+}*/
+//Nullish colansing operator
+
+/*let val1;
+val1= null??undefined??10;
+console.log(val1);*/
+let variance = 100;
+variance <= 80 ?console.log("Price less than 80"):console.log("more than it");//terniary operator
