51bc4d4b594e66a6c5236b93f54676de94070ff4
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-09 07:42:38 +0000

    scope intiatied

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+function one(){
+    const username = "Diamond"
+    //console.log(username);
+
+    function two(){
+        const course = "js";
+        console.log(course);
+        console.log(username)
+    }
+    
+    two();
+}
+one();
