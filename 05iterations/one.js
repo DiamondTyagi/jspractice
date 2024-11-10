ff80bb21697b50fbf45834cb50be383f709bef3b
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-10 07:52:37 +0000

    Loopsdone

--- 05iterations/one.js
+++ 05iterations/one.js
@@ -1,5 +1,6 @@
-for (let i= 0; i < 19; i++) {
-    const element = i;
-    console.log(element);
-    
+for(let i = 0; i<=10;i++){
+    console.log(`outer loop: ${i}`);
+    for(let j = 0; j<=10;j++){
+        console.log(i + `*` + j + `=` + i*j);
+    }
 }
---
+++ 05iterations/two.js
@@ -0,0 +1,11 @@
+let myarr = ['batman', 'diamondhead', 'bigchill']
+let arr = 0;
+while(arr < myarr.length){
+    console.log(`the array tranverse is, ${myarr[arr]}`);
+    arr = arr + 1;
+}
+let score = 1;
+do {
+console.log(`score is, ${score}`);
+score++;
+} while (score <= 12);
