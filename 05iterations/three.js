38b4181f2a85296442a07b145d62d6fa799c331f
Diamond Tyagi <101418307+DiamondTyagi@users.noreply.github.com>
2024-11-10 12:38:22 +0000

    Map complete

--- 05iterations/three.js
+++ 05iterations/three.js
@@ -1,4 +1,12 @@
-const arr = [1,2,3,4,5];
+/*const arr = [1,2,3,4,5];
 for (const nums of arr) {
     console.log(nums);
-}
+}*/
+const map = new Map();
+map.set("IN", "India");
+map.set("US", "United statets of India");
+map.set("Fr", "France");
+//console.log(map);
+for (const [key, three] of map) {
+   console.log(key, "=", three);  
+}//iteration of a map
